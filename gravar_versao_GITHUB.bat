abfaf3174bed1597b8666a53143dc1065fef35c9
FJP <flp-business.strategy@outlook.com>
2025-06-20 17:02:41 -0300

    Atualiza├º├úo estrutural: cria├º├úo das pastas modulares (V 0.0.5)
    V.0.0.5

--- gravar_versao_GITHUB.bat
+++ gravar_versao_GITHUB.bat
@@ -1,4 +1,3 @@
 git add .
-git commit -m "Atualização estrutural: criação das pastas modulares (V 0.0.5)"
-git tag "V 0.0.5"
-git push origin --tags
+git commit -m "Atualização estrutural: criação das pastas modulares (V 0.0.5)" -m "V.0.0.5"
+git push -u origin main --tags
